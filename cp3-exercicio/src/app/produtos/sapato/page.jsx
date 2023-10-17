cab59501263d331e11a8363d734d5d205a6c84e9
GeovanaaProgrammer <rm99646@fiap.com.br>
2023-10-16 22:27:27 -0300

    Formatações de fontes + ajustes paleta de cores

--- cp3-exercicio/src/app/produtos/sapato/page.jsx
+++ cp3-exercicio/src/app/produtos/sapato/page.jsx
@@ -3,14 +3,16 @@ import Link from 'next/link'
 export default function Sapato () {
     return(
       <div>
-        <h1>SAPATO</h1>
+        <h1 className='sapato'>SAPATO</h1>
         <div>
                 <p>R$350,00</p>
                
                 <figure>
                     <Image src="/img/sapato.jpg" alt="Sapato" width={300} height={250}/>
                 </figure>
-                 <p> <Link href="/">BACK TO HOME</Link></p>
+                <div className='retorne'>
+                 <p> <Link href="/">RETORNE A LOJA</Link></p>
+                 </div>
             </div>
       </div>
     )
