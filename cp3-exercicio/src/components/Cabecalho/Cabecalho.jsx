a5920eb0e4ded32ad62804e6777b74bee94e19b2
CalinaThalya <Calina.thalya@cloud.com>
2023-10-17 20:29:12 -0300

    ajustes

--- cp3-exercicio/src/components/Cabecalho/Cabecalho.jsx
+++ cp3-exercicio/src/components/Cabecalho/Cabecalho.jsx
@@ -5,21 +5,23 @@ export default function Cabecalho() {
         <div>
             <header>
                 <nav>
-                    <p>
-                        <Link href="/produtos/bone">BONÉ</Link>
-                    </p>
-                    <p>
-                        <Link href="/produtos/calca">CALÇA</Link>
-                    </p>
-                    <p>
-                        <Link href="/produtos/camisa">CAMISA</Link>
-                    </p>
-                    <p>
-                        <Link href="/produtos/oculos">ÓCULOS</Link>
-                    </p>
-                    <p>
-                        <Link href="/produtos/sapato">SAPATO</Link>
-                    </p>
+                    <div>
+                        <p>
+                            <Link href="/produtos/bone">BONÉ</Link>
+                        </p>
+                        <p>
+                            <Link href="/produtos/calca">CALÇA</Link>
+                        </p>
+                        <p>
+                            <Link href="/produtos/camisa">CAMISA</Link>
+                        </p>
+                        <p>
+                            <Link href="/produtos/oculos">ÓCULOS</Link>
+                        </p>
+                        <p>
+                            <Link href="/produtos/sapato">SAPATO</Link>
+                        </p>
+                    </div>
                 </nav>
             </header>
         </div>
