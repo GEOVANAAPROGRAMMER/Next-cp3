67df3cdfc24b4fde267a6d13c7d0d3c3ba0cb441
anapaulans <rm552513@fiap.com.br>
2023-10-17 21:14:21 -0300

    Componentização

--- cp3-exercicio/src/app/layout.jsx
+++ cp3-exercicio/src/app/layout.jsx
@@ -18,37 +18,8 @@ export default function RootLayout({ children }) {
 
         <Cabecalho />
 
-
         {children}
 
-        <div className="centered-container">
-          <h2>LISTA DE PRODUTOS</h2>
-
-          <div className="card-container">
-
-            <a href="/produtos/calca" className="card-link">
-              <Image src="/img/calca.jpg" alt='calca' width={50} height={50} />
-            </a>
-
-            <a href="/produtos/bone" className="card-link">
-              <Image src="/img/bone.png" alt='bone' width={50} height={50} />
-            </a>
-
-            <a href="/produtos/camisa" className="card-link">
-              <Image src="/img/camisa.png" alt='camisa' width={50} height={50} />
-            </a>
-
-            <a href="/produtos/oculos" className="card-link">
-              <Image src="/img/oculos.jpg" alt='oculos' width={50} height={50} />
-            </a>
-
-            <a href="/produtos/sapato" className="card-link">
-              <Image src="/img/sapato.jpg" alt='sapato' width={50} height={50} />
-            </a>
-
-
-          </div>
-        </div>
       </body>
     </html>
   )
--- cp3-exercicio/src/app/page.jsx
+++ cp3-exercicio/src/app/page.jsx
@@ -1,10 +1,34 @@
 import Link from 'next/link'
 
 export default function () {
-  return(
-    <div>
-      <h1 className='loja'>LOJA DE ITENS</h1>
-      
+  return (
+    <div className="centered-container">
+      <h2>LISTA DE PRODUTOS</h2>
+
+      <div className="card-container">
+
+        <a href="/produtos/calca" className="card-link">
+          <Image src="/img/calca.jpg" alt='calca' width={50} height={50} />
+        </a>
+
+        <a href="/produtos/bone" className="card-link">
+          <Image src="/img/bone.png" alt='bone' width={50} height={50} />
+        </a>
+
+        <a href="/produtos/camisa" className="card-link">
+          <Image src="/img/camisa.png" alt='camisa' width={50} height={50} />
+        </a>
+
+        <a href="/produtos/oculos" className="card-link">
+          <Image src="/img/oculos.jpg" alt='oculos' width={50} height={50} />
+        </a>
+
+        <a href="/produtos/sapato" className="card-link">
+          <Image src="/img/sapato.jpg" alt='sapato' width={50} height={50} />
+        </a>
+
+
+      </div>
     </div>
   )
 }
